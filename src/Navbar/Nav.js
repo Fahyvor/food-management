b198f4d7979aba35e84ae2565072402b82b66384
Fahyvor <favourokafor30@gmail.com>
2023-09-28 19:59:11 +0100

    dropdown

--- src/Navbar/Nav.js
+++ src/Navbar/Nav.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars } from 'react-icons/fa'
 
 const Nav = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className='homepage-nav 
-    bg-white py-2 w-full flex gap-9 justify-center'>
+    <div className='p-3'>
+      <FaBars size={45}  onClick={() => setShow(true)}/>
+    {show ? (<div className='homepage-nav 
+    bg-white py-2 w-full gap-9 flex
+    bg-white absolute items-center
+    flex-col justify-center'>
             <a href='/' className='w-1/5 font-bold
             text-xl'>Home</a>
             <a href='/login' className='w-1/5 font-bold
             text-xl' >Login</a>
             <a href='/register' className='w-1/5 font-bold
             text-xl' >Register</a>
+    </div>
+    ) : null }
     </div>
   )
 }
